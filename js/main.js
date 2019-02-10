0da14e4fba369ff2221a33cf6942b92d63d5f39d
Mirza <mirza@Mirzas-MacBook-Pro.local>
2019-02-10 16:21:52 -0500

    updated

--- js/main.js
+++ js/main.js
@@ -169,15 +169,15 @@
 	};
 
 	// Reflect scrolling in navigation
-	var navActive = function(section) {
+	// var navActive = function(section) {
 
-		var $el = $('#navbar > ul');
-		$el.find('li').removeClass('active');
-		$el.each(function(){
-			$(this).find('a[data-nav-section="'+section+'"]').closest('li').addClass('active');
-		});
+	// 	var $el = $('#navbar > ul');
+	// 	$el.find('li').removeClass('active');
+	// 	$el.each(function(){
+	// 		$(this).find('a[data-nav-section="'+section+'"]').closest('li').addClass('active');
+	// 	});
 
-	};
+	// };
 
 	var navigationSection = function() {
 
@@ -308,3 +308,15 @@
 
 
 }());
+
+
+
+function myFunction() {
+	myVar = setTimeout(showPage, 3000);
+}
+  
+function showPage() {
+	document.getElementById("background").style.display = "none";
+	document.getElementById("myDiv").style.display = "block";
+}
+
